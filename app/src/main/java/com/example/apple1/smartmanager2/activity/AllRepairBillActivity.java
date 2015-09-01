e524a78ec58dc2a6ecf97d50c93af9b61acec2bc
draft <971143573@qq.com>
2015-09-01 08:52:06 +0800

    写写写

--- app/src/main/java/com/example/apple1/smartmanager2/activity/AllRepairBillActivity.java
+++ app/src/main/java/com/example/apple1/smartmanager2/activity/AllRepairBillActivity.java
@@ -1,49 +1,187 @@
 package com.example.apple1.smartmanager2.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.apple1.smartmanager2.R;
+
+import com.example.apple1.smartmanager2.entity.RepairDetail;
+
+import com.example.apple1.smartmanager2.net.GetPicture;
+import com.example.apple1.smartmanager2.net.NetThread;
+import com.example.apple1.smartmanager2.tools.AutoString;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
  * Created by draft on 2015/7/20.
  */
-public class AllRepairBillActivity extends Activity {
-
-    ImageView image=null;
-    EditText editLocation=null,editPhoneNumber=null,editRepairType=null,
-            editSource=null,editRepresentation=null,editRemark=null;
-    ImageButton btnCall=null;
-    Button btnRepair;
+public class AllRepairBillActivity extends Activity implements View.OnClickListener {
+    //申明对象
+    private ImageView image=null;
+    private TextView textLocation=null,textPhoneNumber=null,textRepairType=null,
+            textSource=null,textRepresentation=null,textRemark=null;
+    private ImageButton btnCall=null,btnback=null;
+    private Button btnRepair;
+    private String id;
+    private String url="http://1.smartprotecter.sinaapp.com/sm/service_id01.php";
+    private Handler han,hanGetImage;
+    RepairDetail repairDetail=new RepairDetail();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_repair_bill);
+        //得到id
+        Intent intent=getIntent();
+        id=intent.getStringExtra("extra_data1");
         init();
-
+        getRepairDetail();
+        btnCall.setOnClickListener(this);
+        btnRepair.setOnClickListener(this);
+        btnback.setOnClickListener(this);
     }
 
+
+
+
+
+
+    //得到详细信息
+    private void getRepairDetail() {
+
+        AutoString autoString=new AutoString("s_id",id);
+        String params=autoString.getResult();
+        Log.d("test", "params" + "       " + params);
+        NetThread nt=new NetThread(han,url,params);
+        nt.start();
+
+
+    }
 
     //初始化各个控件
     private void init() {
         //报修的图片
         image=(ImageView)findViewById(R.id.image);
         //各个EditText
-        editLocation=(EditText)findViewById(R.id.edit_location);
-        editPhoneNumber=(EditText)findViewById(R.id.edit_phone_number);
-        editRepairType=(EditText)findViewById(R.id.edit_repair_type);
-        editSource=(EditText)findViewById(R.id.edit_source);
-        editRepresentation=(EditText)findViewById(R.id.edit_representation);
-        editRemark=(EditText)findViewById(R.id.edit_remark);
+        textLocation=(TextView)findViewById(R.id.text_location);
+        textPhoneNumber=(TextView)findViewById(R.id.text_phone_number);
+        textRepairType=(TextView)findViewById(R.id.text_repair_type);
+        textSource=(TextView)findViewById(R.id.text_source);
+        textRepresentation=(TextView)findViewById(R.id.text_representation);
+        textRemark=(TextView)findViewById(R.id.text_remark);
         //打电话的ImageButton
         btnCall=(ImageButton)findViewById(R.id.btn_call);
-        //完成修理的按钮
+        //查看反馈的按钮
         btnRepair=(Button)findViewById(R.id.btn_repair);
+        //返回的按钮
+        btnback=(ImageButton)findViewById(R.id.button_back);
 
+        hanGetImage = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                // TODO Auto-generated method stub
+                Bitmap bitmap = (Bitmap) msg.obj;
+                Log.d("test","bitmap"+"           "+bitmap);
+                super.handleMessage(msg);
+                image.setImageBitmap(bitmap);
+
+            }
+        };
+
+        han = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                String Jsmess =(String) msg.obj;
+                Log.d("test","Jsmess"+Jsmess);
+                super.handleMessage(msg);
+                if (Jsmess.equals("1")){
+                    Toast.makeText(AllRepairBillActivity.this, "连接失败", Toast.LENGTH_SHORT).show();
+                }else {
+                    parseJSONWithJSONObject(Jsmess);
+                    //设置数据
+                    Log.d("test","repairDetail.getImgurl()"+"             "+repairDetail.getImgurl());
+                    GetPicture getPicture=new GetPicture(hanGetImage,repairDetail.getImgurl());
+                    getPicture.start();
+                    textLocation.setText(repairDetail.getLocation());
+                    textPhoneNumber.setText(repairDetail.getPhonenumber());
+                    textRepairType.setText(repairDetail.getType());
+                    textSource.setText(repairDetail.getSource());
+                    textRepresentation.setText(repairDetail.getRepresentation());
+                    textRemark.setText(repairDetail.getRemark());
+                }
+            }
+
+            private void parseJSONWithJSONObject(String Jsmess) {
+                // TODO Auto-generated method stub
+
+                try {
+
+                    JSONObject temp = new JSONObject(Jsmess);
+                    String reason = temp.getString("reason");
+                    repairDetail.setRepresentation(reason);
+                    String phoneNumber=temp.getString("phonenumber");
+                    repairDetail.setPhonenumber(phoneNumber);
+                    String location = temp.getString("address");
+                    repairDetail.setLocation(location);
+                    String bug=temp.getString("origin");
+                    repairDetail.setSource(bug);
+                    String imageUrl=temp.getString("image_path");
+                    repairDetail.setImgurl(imageUrl);
+                    String type=temp.getString("types");
+                    repairDetail.setType(type);
+                    String memo=temp.getString("memo");
+                    repairDetail.setRemark(memo);
+
+
+
+
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Log.d("test","出错了");
+                    e.printStackTrace();
+                }
+
+            }
+
+        };
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_call:
+                String number = textPhoneNumber.getText().toString();
+                //用intent启动拨打电话
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + number));
+                startActivity(intent);
+                break;
+            case R.id.btn_repair:
+                break;
+            case R.id.button_back:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/apple1/smartmanager2/fragment/MyRepairListFragment.java
+++ app/src/main/java/com/example/apple1/smartmanager2/fragment/MyRepairListFragment.java
@@ -3,15 +3,38 @@ package com.example.apple1.smartmanager2.fragment;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.apple1.smartmanager2.Application.ManagerData;
 import com.example.apple1.smartmanager2.R;
+import com.example.apple1.smartmanager2.activity.FinishedRepairBillActivity;
+import com.example.apple1.smartmanager2.activity.MyRepairBillActivity;
+import com.example.apple1.smartmanager2.adapter.MyRepairAdapter;
+import com.example.apple1.smartmanager2.adapter.RepairRecordAdapter;
+import com.example.apple1.smartmanager2.entity.RepairRecord;
+import com.example.apple1.smartmanager2.net.NetThread;
+import com.example.apple1.smartmanager2.tools.AutoString;
+import com.example.apple1.smartmanager2.tools.SlidingMenu;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class MyRepairListFragment extends Fragment {
@@ -19,6 +42,12 @@ public class MyRepairListFragment extends Fragment {
 	public Activity activity;
 	private View view;
 	private View mMainView;
+	private ListView listView;
+	private MyRepairAdapter adapter;
+	private ArrayList<RepairRecord> repairRecordList;
+	private Handler han;
+	private String url="http://1.smartprotecter.sinaapp.com/sm/manager_id01.php";
+	private ManagerData managerData;
 	@Override
 	/**
 	 * 初始化操作
@@ -40,7 +69,75 @@ public class MyRepairListFragment extends Fragment {
 		super.onCreateView(inflater, container, savedInstanceState);
 		view = inflater.inflate(R.layout.fragment_my_repair, container,
 				false);
+		//实例化repairRecordList
+		repairRecordList=new ArrayList<RepairRecord>();
+		//获得manageData
+		managerData= (ManagerData) getActivity().getApplication();
+		//初始化ListView
+		listView= (ListView) view.findViewById(R.id.listview_record);
+		// listView.setAdapter(adapter);
+		han = new Handler(){
+			@Override
+			public void handleMessage(Message msg) {
+				String Jsmess =(String) msg.obj;
+				Log.d("test", "Jsmess" + Jsmess);
+				super.handleMessage(msg);
+				if (Jsmess.equals("1")){
+					Toast.makeText(context, "连接失败", Toast.LENGTH_SHORT).show();
+				}else {
+					parseJSONWithJSONObject(Jsmess);
+				}
+			}
 
+			private void parseJSONWithJSONObject(String Jsmess) {
+				// TODO Auto-generated method stub
+				JSONArray arr = null;
+				try {
+					arr = new JSONArray(Jsmess);
+					Log.d("test", "arr" + arr);
+					for (int i = 0; i < arr.length(); i++) {
+						JSONObject temp = (JSONObject) arr.getJSONObject(i);
+						String sId = temp.getString("s_id");
+						String phoneNumber=temp.getString("phonenumber");
+						String location = temp.getString("address");
+						String bug=temp.getString("origin");
+						String imageUrl=temp.getString("image_path");
+						//生成repairRecord
+						RepairRecord repairRecord=new RepairRecord(sId,location,phoneNumber,bug,imageUrl);
+						Log.d("test", " repairRecord.toString()" +"               "+ repairRecord.toString());
+						//加入列表
+						Log.d("test", "repairRecordList" + repairRecordList);
+						repairRecordList.add(repairRecord);
+
+					}
+
+
+
+					Log.d("test","activity"+"           "+activity);
+					adapter=new MyRepairAdapter (activity,R.layout.repair_item,repairRecordList);
+					listView.setAdapter(adapter);
+					//Log.d("test", "exapmple" + example);
+					listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+						public void onItemClick(AdapterView<?> parent, View view, int position, long id1) {
+							RepairRecord repairRecord =repairRecordList.get(position);
+							Intent intent = new Intent(activity, MyRepairBillActivity.class);
+							intent.putExtra("extra_data1", repairRecord.getS_id());
+							startActivity(intent);
+						}
+					});
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		};
+		Log.d("test","managerData.getManagerId()"+"       "+managerData.getManagerId());
+		AutoString autoString=new AutoString("m_id",managerData.getManagerId());
+		String params=autoString.getResult();
+		Log.d("test","params"+"       "+params);
+		NetThread nt=new NetThread(han,url,params);
+		nt.start();
 		return view;
-	}
+}
 }
