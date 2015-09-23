0ab42dad542e74b24705634dd0d281a3e263b3ec
zhangwen <646689983@qq.com>
2015-09-23 21:24:26 +0800

    22

---
+++ app/src/main/java/com/example/apple1/smartmanager2/Application/GetService.java
@@ -0,0 +1,15 @@
+package com.example.apple1.smartmanager2.Application;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Huhu on 9/23/15.
+ */
+public class GetService  extends Service{
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
